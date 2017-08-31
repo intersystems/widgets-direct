df126c63428f439b544914180830f749e364b273
Orion Correa <ocorrea@intersystems.com>
2017-08-30 23:52:11 -0400

    Up to stage 7

--- WidgetsDirect/CSP/widgetsdirect/widgetmaster.js
+++ WidgetsDirect/CSP/widgetsdirect/widgetmaster.js
@@ -1,22 +1,3 @@
 angular
   .module('WidgetsDirect', ['ngMaterial'])
-   .controller('PageController', ['$q', '$scope', '$timeout', '$http', '$log', function($q, $scope, $timeout, $http, $log) {
 	
-	$scope.message = "";
-	$scope.sortAsc = true;
-	
-	
-	
-	$http.get('/widgetsdirect/rest/Chris').then(
-	  function(response) { //success
-	    $scope.message = response.data.Message;
-	    $scope.widgets = response.data.Widgets;
-      }
-     ,function(response) { //failure
-	   $scope.message = "Couldn't get data :(";
-       }
-      );
-	   
-   		}
-   	  ]
-   )
